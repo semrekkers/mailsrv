42f93c3e04afb01715a918416ffcb145e21c47ae
Sem Rekkers <rekkers.sem@gmail.com>
2018-02-19 12:30:03 +0100

    db: Update seed and queries

--- database/seed.sql
+++ database/seed.sql
@@ -2,14 +2,17 @@ CREATE TABLE `virtual_domains` (
     `id` int NOT NULL AUTO_INCREMENT,
     `name` varchar(50) NOT NULL,
 
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `virtual_users` (
     `id` int NOT NULL AUTO_INCREMENT,
     `domain_id` int NOT NULL,
-    `password` varchar(106) NOT NULL,
     `email` varchar(120) NOT NULL,
+    `password` varchar(106) NOT NULL,
+    `send_only` tinyint(1) NOT NULL DEFAULT 0,
+    `active` tinyint(1) NOT NULL DEFAULT 1,
 
     PRIMARY KEY (`id`),
     UNIQUE KEY (`email`),
@@ -25,5 +28,6 @@ CREATE TABLE `virtual_aliases` (
 
     PRIMARY KEY (`id`),
     KEY (`domain_id`),
+    KEY (`source`),
     FOREIGN KEY (`domain_id`) REFERENCES `virtual_domains`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
